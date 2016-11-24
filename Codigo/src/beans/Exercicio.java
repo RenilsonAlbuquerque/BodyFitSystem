d183b3f90386d4d480d8fca28b8b310c21e5d3a1
Davi <Davi@DESKTOP-PB2OSN8>
2016-11-24 16:09:19 -0300

    classe basica de exercicio e treino.

--- Codigo/src/beans/Exercicio.java
+++ Codigo/src/beans/Exercicio.java
@@ -1,7 +1,52 @@
 package beans;
 
 public class Exercicio {
-	private int codTp;
 	private String nome;
+	private String carga;
+	private int intervalo;
+	private int repeticao;
+	private int codigoEp;
+	
+	
+	
+	public Exercicio(String nome, String carga, int intervalo, int repeticao, int codigoEp) {
+		super();
+		this.setNome(nome);
+		this.setCarga(carga);
+		this.setIntervalo(intervalo);
+		this.setRepeticao(repeticao);
+		this.setCodigoEp(codigoEp);
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCarga() {
+		return carga;
+	}
+	public void setCarga(String carga) {
+		this.carga = carga;
+	}
+	public int getIntervalo() {
+		return intervalo;
+	}
+	public void setIntervalo(int intervalo) {
+		this.intervalo = intervalo;
+	}
+	public int getRepeticao() {
+		return repeticao;
+	}
+	public void setRepeticao(int repeticao) {
+		this.repeticao = repeticao;
+	}
+	public int getCodigoEp() {
+		return codigoEp;
+	}
+	public void setCodigoEp(int codigoEp) {
+		this.codigoEp = codigoEp;
+	}
+
 	
 }
--- Codigo/src/beans/Treino.java
+++ Codigo/src/beans/Treino.java
@@ -1,5 +1,35 @@
 package beans;
 
-public class Treino {
+import java.util.ArrayList;
 
+public class Treino {
+	private int codTp;
+	private String nome;
+	private ArrayList<Exercicio>exerciciosArray;
+	
+	public Treino(int codTp, String nome, ArrayList<Exercicio>exerciciosArray) {
+		super();
+		this.codTp = codTp;
+		this.nome = nome;
+		this.exerciciosArray = new ArrayList<Exercicio>();
+	}
+	public int getCodTp() {
+		return codTp;
+	}
+	public void setCodTp(int codTp) {
+		this.codTp = codTp;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public ArrayList<Exercicio> getExerciciosArray() {
+		return exerciciosArray;
+	}
+	public void setExerciciosArray(ArrayList<Exercicio> exerciciosArray) {
+		this.exerciciosArray = exerciciosArray;
+	}
+	
 }
